ae663efcb5b071cd8b9651baada0c99fdb916c82
徐宛谦 <1151331098@qq.com>
2023-10-08 15:37:07 +0800

    暂存，准备开始基于完整IOC（包括依赖注入等功能）的代码实现的大改动。

---
+++ rpcIoc/src/main/java/github/xunolan/rpcproject/annotation/Component.java
@@ -0,0 +1,4 @@
+package github.xunolan.rpcproject.annotation;
+
+public @interface Component {
+}
--- rpcIoc/src/main/java/github/xunolan/rpcproject/annotation/server/Qualifier.java
+++
@@ -1,4 +0,0 @@
-package github.xunolan.rpcproject.annotation.server;
-
-public @interface Qualifier {
-}
